f15540f83bdbf45c8bed21e43c7b0cb3496f0c81
QueenBu <bianca.Postdam@gmail.com>
2020-05-19 19:59:44 +0200

    Committed

---
+++ src/main/java/com/DBP/DBP/DbpApplication.java
@@ -0,0 +1,13 @@
+package com.DBP.DBP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbpApplication.class, args);
+	}
+
+}
